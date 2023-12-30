e7912da8fecd15c9f2e4b8b9478359e332b104ad
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2023-12-30 01:31:42 +0100

    Update second.py

--- second.py
+++ second.py
@@ -110,8 +110,7 @@ if Feedback == "fuck":
    st.warning("woah")
 
 st.subheader("About me")
-st.write("Hello, I am a 14 year old, still, python learning student. This is the first python project I've ever made.
-")
+st.write("Hello, I am a 14 year old, still, python learning student. This is the first python project I've ever made.")
 
 
 
