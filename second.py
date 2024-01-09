2077193cd183ebc05d3c466878e5e3660358abae
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-09 22:29:04 +0100

    Update second.py

--- second.py
+++ second.py
@@ -128,8 +128,9 @@ if selected == "Home":
          st.title("Home")
          st.subheader("Welcome to my wonderful website, I hope you enjoy!")
          st.write("#")
-         st.write("This is where I store all my projects")
-         st.write("I hope you enjoy what I made, dont be scared to leave some feedback behind")
+         st.write("This is where I store all my projects, I make these project outside of school as a hobby. 
+         I'm excited to learn more as I go on with making stuff! Maybe even learn other programming languages.")
+         st.write("All the projects I've made so far are made fully in python")
          st.write("#")
          st.text_input("What's your name?")
    
