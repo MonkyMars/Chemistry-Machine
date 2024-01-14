5b71cbe61c7760379c7b1f5845f0b0a0dabf30d5
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-14 21:23:03 +0100

    Update second.py

--- second.py
+++ second.py
@@ -135,7 +135,7 @@ if selected == "Home":
          st.write("This is where I store all my projects, I make these project outside of school as a hobby. I'm excited to learn more as I go on with making stuff! Maybe even learn other programming languages.")
          st.write("All the projects I've made so far are made fully in python")
          st.write("#")
-        name = st.text_input("What's your name?")
+         name = st.text_input("What's your name?")
    
 
 if selected == "About me":
