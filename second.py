94d743a9caf29afb2b6d06cbee0d2ddf8945bbfc
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-14 21:27:36 +0100

    Update second.py

--- second.py
+++ second.py
@@ -176,7 +176,7 @@ if Name:
     Name = "N/A"
 
 with open('data.txt','w') as file: 
-    file.write(Name\nQuestion1\nQuestion2\nFeedback\n)
+    file.write(Name, Question1, Question2, Feedback\n)
               
 
               
