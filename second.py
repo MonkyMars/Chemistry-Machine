3d338f439b3e53c497918a7e1c96ca7701abedc4
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-18 23:41:54 +0100

    Update second.py

--- second.py
+++ second.py
@@ -147,7 +147,7 @@ if selected == "Home":
     else:
         name = "" + st.session_state['name_input']
     st.title("Home")
-    st.subheader(f"Welcome{name}, to my wonderful website, I hope you enjoy!")
+    st.subheader(f"Welcome{ name}, to my wonderful website, I hope you enjoy!")
     st.write("#")
     st.write("This is where I store all my projects, I make these project outside of school as a hobby. I'm excited to learn more as I go on with making stuff! Maybe even learn other programming languages.")
     st.write("All the projects I've made so far are made fully in python")
