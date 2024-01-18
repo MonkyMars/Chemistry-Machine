7065000b744225006f7244b024e2622788accb91
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-18 11:27:34 +0100

    Update second.py

--- second.py
+++ second.py
@@ -17,7 +17,7 @@ if selected == "Chemistry Machine":
     Methaan_moleculen = "C", "H4"
     Zwavelzuur_moleculen = "H2", "S", "O4"
     Zwaveldioxide_moleculen = "S", "O2"
-    zuurstof_moleculen = "O",
+    Zuurstof_moleculen = "O",
     #alleengewicht
     H_gewicht = 1.008
     O_gewicht = 16.00
@@ -75,7 +75,7 @@ if selected == "Chemistry Machine":
     if Question1 == "sulphuric dioxide" and Question2 == "Molecular formula":
         answer = Zwaveldioxide_moleculen
     if Question1 == "oxygen" and Question2 == "Molecular formula":
-        answer = zuurstof_moleculen
+        answer = Zuurstof_moleculen
 
 #gewichten
 if selected == "Chemistry Machine":
