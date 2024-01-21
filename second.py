66633b01eabd704388416ac96c980e9123ee3fad
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-21 15:19:40 +0100

    Update second.py

--- second.py
+++ second.py
@@ -242,6 +242,8 @@ if selected == "About me":
             st.success("Thank you for contacting me, I will get back to you shortly!")
         except:
             st.error("Unable to contact, try again later")
+        else:
+            st.success("Succesfully contacted")
     
         
         
