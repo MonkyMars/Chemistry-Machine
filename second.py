586ebed4cf11aa7bef57bf7cfb7194058003e7c3
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-22 14:14:52 +0100

    Update second.py

--- second.py
+++ second.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
-import duckdb
+
 st.set_page_config(page_title="Chemistry Machine", page_icon=":test_tube:")
 
 with (st.sidebar):
