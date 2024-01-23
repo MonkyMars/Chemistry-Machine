ac8308bbadb05bf2e0a5fbd102b7c16b31ec9673
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-23 18:14:10 +0100

    Update second.py

--- second.py
+++ second.py
@@ -20,18 +20,6 @@ if selected == "Home":
         with modal.container():
             st.write("Text goes here")
 
-            html_string = '''
-            <h1>HTML string in RED</h1>
-
-            <script language="javascript">
-              document.querySelector("h1").style.color = "red";
-            </script>
-            '''
-            components.html(html_string)
-
-            st.write("Some fancy text")
-            value = st.checkbox("Check me")
-            st.write(f"Checkbox checked: {value}")
 #######
 if selected == "Chemistry Machine":
     Water_moleculen = "H2", "0"
