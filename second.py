2263174a72830574428f7cbaac8da3308f605648
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-28 10:50:10 +0100

    Update second.py

--- second.py
+++ second.py
@@ -306,7 +306,7 @@ if selected == "Chemistry Machine":
     if Submit1:
         if Q1 and Q2:
             try:
-                time.sleep(1)
+                time.sleep(0.5)
                 conn.update(worksheet="data", data=updated_df)
             except: 
                 st.error("unexpected error happened")
