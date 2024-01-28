1d1839767c3201710c086e7a4a88d17c01a4880b
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-28 23:51:52 +0100

    Update second.py

--- second.py
+++ second.py
@@ -750,7 +750,8 @@ if selected == "Physics Machine":
          with column6:
              f = st.button("placeholder", key = "1c")
     if c:
-        modal = Modal(key="Key123", title="Calculating Speed")
+        modal.open()
+    if modal.is_open:
         with modal.container():
             unit = st.selectbox("Select unit", ("","KM/H", "MPH", "M/S"))
             ask1 = st.text_input("Enter distance")
