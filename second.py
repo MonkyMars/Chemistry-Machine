1b8ed829188a144040a1aa41a0ad312d8ca97058
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-28 23:48:47 +0100

    Update second.py

--- second.py
+++ second.py
@@ -752,7 +752,7 @@ if selected == "Physics Machine":
     if c:
         modal = Modal(key="Key123", title="Calculating Speed")
         with modal.container():
-            unit = st.selectbox("Select unit", ("","KM/H", "MPH", "M/S")
+            unit = st.selectbox("Select unit", ("","KM/H", "MPH", "M/S"))
             ask1 = st.text_input("Enter distance")
             ask2 = st.text_input("Enter time") 
         
