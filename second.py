ffe016a3b566c65688f83be880ecf4cdf8c3ecd2
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-01-29 21:09:42 +0100

    Update second.py

--- second.py
+++ second.py
@@ -748,7 +748,7 @@ if selected == "Physics Machine":
         st.subheader("Calculating Gravity")
         Q3 = st.text_input("insert mass in KG")
         if Q3:
-            M = int(Q3)
+            M = float(Q3)
             G = 9.81
             answer = M * G
             st.write(f"Your answer is {answer} Newton")
